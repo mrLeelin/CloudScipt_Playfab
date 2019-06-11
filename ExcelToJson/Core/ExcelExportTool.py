ba83b96cd6ea332ce8eafc147f7fc7c20e71873a
780492971@qq.com <780492971@qq.com>
2019-06-11 13:58:42 +0800

    更新表转json

--- ExcelToJson/Core/ExcelExportTool.py
+++ ExcelToJson/Core/ExcelExportTool.py
@@ -4,10 +4,11 @@ import os
 import sys
 import codecs
 import xlrd #http://pypi.python.org/pypi/xlrd
-import json
 import time
 import ExcelInfo
 import importlib
+import json5
+import json
 importlib.reload(sys)
 
 #获取相对路径下的所有文件名
@@ -18,9 +19,24 @@ def all_path(dirname):
             apath = os.path.join(maindir, filename)
             result[filename]=apath
     return result
+def ParseWrite(write):
+    str_write=str(write)
+    str_list=str_write.split('\"')
+    str_new=[]
+    list_last=str_list[len(str_list)-1]
+    for s in str_list:
+        add=""
+        if s != list_last:
+            add='\\"'
+        str_new.append(str(s)+add)
+    str_write="".join(str_new)
+    str_write= str(str_write).strip().strip('[]')
+    str_write='{\" %s \":\"%s\" }'%(key,str_write)
+    return str_write
 
 if __name__ == '__main__':
-    config=json.load(open( 'Config.json','r'))
+   # config=json.load(open( 'Config.json','r'))
+    config=json5.load(open('Config.json','r',encoding='UTF-8'))
     paths=all_path(config["srcFolder"])
     for key in paths:
         if key[0]=='~':#忽略打开的Excel文件产生的临时文件
@@ -35,28 +51,19 @@ if __name__ == '__main__':
                 outPath=config["destFolder"]+'/'+ key+'.json'
                 with open(outPath,'w',encoding='UTF-8') as fileobject:
                     str_write=''
-                    if config["format"]==True:
-                        str_write=str(json.dumps(finalJsons[key],indent=4,ensure_ascii=False))
+                    d=dict(config['format'])
+                    if d.keys().__contains__(key):
+                        if d[key]==True:
+                            str_write=str(json.dumps(finalJsons[key],indent=4,ensure_ascii=False))
+                            if config["ignoreAnceps"]==True:
+                                str_write=str(str_write).strip().strip('[]')
+                        else:
+                            str_write=str(json.dumps(finalJsons[key],ensure_ascii=False))
+                            str_write=ParseWrite(str_write) 
                     else:
-                        str_write=str(json.dumps(finalJsons[key],ensure_ascii=False))
-                    
-                    if config["format"]==False :
-                        str_list=str_write.split('\"')
-                        str_new=[]
-                        list_last=str_list[len(str_list)-1]
-                        for s in str_list:
-                            add=""
-                            if s != list_last:
-                                add='\\"'
-                            str_new.append(str(s)+add)
-                            
-                        str_write="".join(str_new)
-                        str_write= str(str_write).strip().strip('[]')
-                        str_write='{\" %s \":\"%s\" }'%(key,str_write)
-                    
+                        str_write=str(json.dumps(finalJsons[key],indent=4,ensure_ascii=False))
                     fileobject.write(str_write)
                     print('exported json  -->  '+outPath)
-            print()
-
+            print() 
     print("All OK")
-    exit(0)
+    exit(0)     
