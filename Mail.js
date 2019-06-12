3edb32a03966aa53fa1b135db119225d4d5f4c1f
Lin Mac <li18202213260@gmail.com>
2019-06-12 16:04:56 +0800

    add mail system`

---
+++ Mail.js
@@ -0,0 +1,135 @@
+handlers.GetMails = clientGetMails;
+handlers.RmMails = clientRmEmails;
+var ItemType;
+(function (ItemType) {
+    ItemType[ItemType["Currency"] = 0] = "Currency";
+    ItemType[ItemType["Item"] = 1] = "Item";
+})(ItemType || (ItemType = {}));
+function clientGetMails(args) {
+    var mails = getMails(currentPlayerId);
+    if (mails == null || mails.length <= 0) {
+        return {
+            id: Func_Code.SC_GET_MAILS,
+            Count: 0,
+        };
+    }
+    mails.sort(function (a, b) {
+        return a.TimeStamp - b.TimeStamp;
+    });
+    var ret = {};
+    ret.Mails = mails;
+    ret.Count = mails.length;
+    return ret;
+}
+function clientRmEmails(args) {
+    if (!rmMails(currentPlayerId, args.MailIds)) {
+        log.error('you remove mail is invaild');
+        return null;
+    }
+    return {
+        id: Func_Code.SC_RM_MAILS
+    };
+}
+function SendToEmail(id, itemType, itemId, count, sender) {
+    if (sender == null) {
+        var userInfo = server.GetUserAccountInfo({
+            PlayFabId: currentPlayerId
+        }).UserInfo;
+        sender = {
+            Name: userInfo.TitleInfo.DisplayName,
+            ImageUrl: userInfo.TitleInfo.AvatarUrl,
+            Level: getLevel(currentPlayerId)
+        };
+    }
+    var mail = {
+        Sender: sender,
+        ItemId: itemId,
+        ItemCount: count,
+        ItemType: itemType,
+        Count: itemId == null ? 0 : itemId.length
+    };
+    if (!addMail(id, mail)) {
+        log.error('you send Email is invaild');
+    }
+}
+function SendOneItemToEmail(id, itemType, itemId, count, sender) {
+    SendToEmail(id, [itemType], [itemId], [count], sender);
+}
+function getMails(playId) {
+    var data = server.GetUserData({
+        PlayFabId: playId,
+        Keys: [KEY_Mail]
+    }).Data;
+    if (data == null || !data.hasOwnProperty(KEY_Mail)) {
+        return null;
+    }
+    var mails = JSON.parse(data[KEY_Mail].Value);
+    if (mails == null || mails.length <= 0) {
+        return null;
+    }
+    return mails;
+}
+function addMail(playId, mail) {
+    var _a;
+    if (mail.Sender == null) {
+        log.error("you Sender is invaild");
+        return false;
+    }
+    var mails = getMails(playId);
+    var id = 0;
+    if (mails != null && mails.length > 0) {
+        mails.sort(function (a, b) {
+            return b.MailId - a.MailId;
+        });
+        id = mails[0].MailId + 1;
+    }
+    else {
+        mails = [];
+    }
+    mail.MailId = id;
+    mail.TimeStamp = GetTimeStamp();
+    mails.push(mail);
+    var json_text = JSON.stringify(mails);
+    server.UpdateUserData({
+        PlayFabId: playId,
+        Data: (_a = {}, _a[KEY_Mail] = json_text, _a)
+    });
+    return true;
+}
+function rmMails(playId, ids) {
+    var _a;
+    if (ids == null || ids.length <= 0) {
+        log.error("you id is invaild.");
+        return false;
+    }
+    var mails = getMails(playId);
+    if (mails == null) {
+        return false;
+    }
+    var rmMails = [];
+    for (var _i = 0, ids_1 = ids; _i < ids_1.length; _i++) {
+        var id = ids_1[_i];
+        var mail = null;
+        for (var _b = 0, mails_1 = mails; _b < mails_1.length; _b++) {
+            var m = mails_1[_b];
+            if (m.MailId == id) {
+                mail = m;
+                break;
+            }
+        }
+        if (mail == null) {
+            return false;
+        }
+        rmMails.push(mail);
+    }
+    for (var _c = 0, rmMails_1 = rmMails; _c < rmMails_1.length; _c++) {
+        var m = rmMails_1[_c];
+        var index = mails.indexOf(m);
+        mails.splice(index, 1);
+    }
+    server.UpdateUserData({
+        PlayFabId: playId,
+        Data: (_a = {}, _a[KEY_Mail] = JSON.stringify(mails), _a)
+    });
+    return true;
+}
---
+++ Src/Mail.ts
@@ -0,0 +1,191 @@
+handlers.GetMails=clientGetMails;
+handlers.RmMails=clientRmEmails;
+
+ interface IGetMailsResult extends IResult{
+     Count:number;
+     Mails?:IMail[];
+}
+
+interface IRmMailsResult extends IResult{
+
+}
+
+interface IMail{
+    MailId?:number;
+    TimeStamp?:number;
+    Sender:ISender;
+    ItemId?:number[];
+    ItemCount?:number[]; 
+    ItemType?:ItemType[];
+    Count:number;
+}
+
+interface ISender{
+    Name:string;
+    ImageUrl:string;
+    Level:number;
+}
+enum ItemType{
+
+    Currency=0,
+    Item=1
+}
+
+interface IClientRmEmailsRequest{
+    MailIds:number[];
+}
+
+function clientGetMails(args:any):IGetMailsResult{
+
+    let mails=getMails(currentPlayerId);
+    
+    if(mails==null||mails.length<=0){
+        return {
+            id:Func_Code.SC_GET_MAILS,
+            Count:0,
+        };
+    }
+    //按照时间排序
+    mails.sort(function(a:IMail,b:IMail){
+        return a.TimeStamp-b.TimeStamp;
+    })
+    let ret:IGetMailsResult=<IGetMailsResult>{};
+    ret.Mails=mails;
+    ret.Count=mails.length
+    return ret;
+}
+
+function clientRmEmails(args :IClientRmEmailsRequest):IRmMailsResult{
+
+    if(!rmMails(currentPlayerId,args.MailIds)){
+        log.error('you remove mail is invaild');
+        return null;
+    }
+     return{
+         id:Func_Code.SC_RM_MAILS
+     }
+}
+
+/**
+ * 
+ * @param id 接受者
+ * @param itemType 物品类型
+ * @param itemId 物品Id
+ * @param count 物品数量
+ * @param sender 发送人 
+ */
+function SendToEmail( id: string,itemType?:ItemType[], itemId?:number[],count?:number[],sender?:ISender) {
+
+    if(sender==null){
+       let userInfo= server.GetUserAccountInfo({
+            PlayFabId:currentPlayerId
+        }).UserInfo;
+
+        sender={
+            Name:userInfo.TitleInfo.DisplayName,
+            ImageUrl:userInfo.TitleInfo.AvatarUrl,
+            Level:getLevel(currentPlayerId)
+        }
+    }
+    let mail:IMail={
+        Sender:sender,
+        ItemId:itemId,
+        ItemCount:count,
+        ItemType:itemType,
+        Count:itemId==null?0:itemId.length
+    };
+    if(!addMail(id,mail)){
+        log.error('you send Email is invaild');
+    }    
+}
+/**
+ * 
+ * @param id 
+ * @param itemType 
+ * @param itemId 
+ * @param count 
+ * @param sender Null 就是谁发写谁
+ */
+function SendOneItemToEmail(id:string,itemType:ItemType,itemId:number,count:number,sender?:ISender){
+
+    SendToEmail(id,[itemType],[itemId],[count],sender);
+}
+
+function getMails(playId:string):IMail[]{
+
+   let data= server.GetUserData({
+        PlayFabId:playId,
+        Keys:[KEY_Mail]
+    }).Data;
+    
+    if(data==null||!data.hasOwnProperty(KEY_Mail)){
+        return null;
+    }
+    let mails:IMail[]= JSON.parse(data[KEY_Mail].Value);
+    if(mails==null||mails.length<=0){
+        return null;
+    }
+    return mails;
+}
+
+function addMail(playId:string, mail:IMail):boolean{
+
+    if(mail.Sender==null){
+        log.error("you Sender is invaild");
+        return false;
+    }
+    let mails:IMail[]=getMails(playId);
+    let id:number=0;
+    if(mails!=null&&mails.length>0){
+        mails.sort(function(a:IMail,b:IMail){
+            return b.MailId-a.MailId;
+        });
+        id=mails[0].MailId+1;
+    }else{
+        mails=[]
+    }
+    mail.MailId=id;
+    mail.TimeStamp=GetTimeStamp();
+    mails.push(mail);
+    let json_text=  JSON.stringify(mails);
+    server.UpdateUserData({
+        PlayFabId:playId,
+        Data:{[KEY_Mail]:json_text}
+    })
+    return true;
+}
+
+function rmMails(playId:string, ids:number[]):boolean{
+
+    if(ids==null||ids.length<=0){
+        log.error("you id is invaild.");
+        return false;
+    }
+    let mails:IMail[]=getMails(playId);
+    if(mails==null){
+        return false;
+    }
+    let rmMails:IMail[]=[];
+    for (const id of ids) {
+        let mail:IMail=null;
+        for (const m of mails) {
+            if(m.MailId==id){
+                mail=m;
+                break;
+            }
+        }
+        if(mail==null){
+            return false;
+        }
+        rmMails.push(mail);
+    }
+    for (const m of rmMails) {
+        let index:number=mails.indexOf(m);
+        mails.splice(index,1);
+    }
+    server.UpdateUserData({
+        PlayFabId:playId,
+        Data:{[KEY_Mail]:JSON.stringify(mails)}
+    })
+    return true;
+}
